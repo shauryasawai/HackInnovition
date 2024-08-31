2836988934f312084081ac776037635034875429
Shreyaskr1409 <shreyaskr.0914@gmail.com>
2024-08-31 12:29:38 +0530

    Svelte reinitialized

---
+++ frontend/src/lib/my_components/navbar.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Button from "$lib/components/ui/button/button.svelte";
+
+</script>
+<div class="fixed justify-center items-center flex w-full h-20 bg-white">
+    <p>Hello world</p>
+    <Button>Hello World</Button>
+</div>
---
+++ frontend/src/routes/home/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import Navbar from "$lib/my_components/navbar.svelte";
+</script>
+
+<div id="canvas" class="">
+    <Navbar></Navbar>
+</div>
